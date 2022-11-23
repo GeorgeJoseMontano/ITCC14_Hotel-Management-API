07929a8a3c5b6c705806873a07d686e54e6e35d0
George Jose Montano <yoshilute.248@gmail.com>
2022-11-23 19:32:50 +0800

    fixed it

--- main.py
+++ main.py
@@ -10,17 +10,9 @@ parser.add_argument('room_id', type=int, help='Room ID of the guest')
 
 
 rooms = {
-    101: {'fname':'George Jose Montano', "guest_lname":"Montano", "room_id":101},
+    101: {'fname':'George Jose', "guest_lname":"Montano", "room_id":101},
     102: {'fname':'John Doe'},
 }
-
-class HelloWorld(Resource):
-    def get(self):
-        return jsonify({'hello': 'world'})
-
-    def post(self):
-        some_json = request.get_json()
-        return jsonify({'you sent': some_json}), 201
 
 class Guest(Resource):
     def get(self, room_id):
@@ -29,8 +21,8 @@ class Guest(Resource):
     def put(self, room_id):
         args = parser.parse_args()
         fname = {'fname': args['fname']}
-        rooms[room_id].update(fname)
-        return fname, 201
+        rooms[room_id] = args
+        return rooms[room_id], 201
 
     def delete(self, room_id):
         del rooms[room_id]
@@ -40,16 +32,8 @@ class Guest_Base(Resource):
     def get(self):
         return rooms
 
-class Booking(Resource):
-    def post(self, room_id, fname):
-        rooms[room_id] = {'guest_fname': fname}
-        return jsonify(rooms[room_id])
-        
-
-api. add_resource(HelloWorld, '/')
 api. add_resource(Guest_Base, '/guest/')
 api. add_resource(Guest, '/guest/<int:room_id>')
-api. add_resource(Booking, '/booking')
 
 if __name__ == '__main__':
     app.run(debug=True)

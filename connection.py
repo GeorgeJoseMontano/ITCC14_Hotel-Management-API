986eacf3b476c2bf70fd69bdfc631f5e47fb6cde
George Jose Montano <yoshilute.248@gmail.com>
2022-12-15 17:59:41 +0800

    change db

--- connection.py
+++ connection.py
@@ -2,7 +2,7 @@ import pymongo
 from pymongo import MongoClient
 
 # Connect to the MongoDB, change the connection string per your MongoDB environment
-cluster = MongoClient("mongodb+srv://yoshilute:rsMQmM1gPzDpmNId@cluster0.xx477oe.mongodb.net/?retryWrites=true&w=majority")
+cluster = MongoClient("mongodb://localhost:27017")
 db = cluster["itcc14"]
 guest_collection = db["guest_info"]
 user_collection = db["user_info"]
